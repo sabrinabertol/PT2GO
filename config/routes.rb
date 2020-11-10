d508b12bece3491426849075934a176c724a91ff
ruben serrano <rubserr@outlook.com>
2020-11-10 15:33:55 +0000

    classes listed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'session#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show]
+
+  resources :sessions, only: [:new, :create, :destroy, :show]
+
+  resources :sessions do
+    resources :reviews, only: [:new, :create, :destroy]
+    resources :bookings, only: [:new, :create, :destroy]
+  end
+
 end
