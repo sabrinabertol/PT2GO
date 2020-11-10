976fde1a39d9a14ca45ce3dd667d3fdc76629944
luis domingues <dominguesfluis@gmail.com>
2020-11-10 15:11:16 +0000

    added edit data fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :email, :pt])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone, :email, :pt, :specialty])
   end
 end
