b45621cc9b9e691a9402a00f3debb5837c815a50
Sabrina Bertol <bertol.sabrina@gmail.com>
2020-11-11 14:07:08 +0000

    created homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :email, :pt])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone, :email, :pt, :specialty])
+  end
 
   def home
   end
